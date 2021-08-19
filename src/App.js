c037bac6129bcd93f9d1887558ebc9be8894118f
git-pushkar <86099012+git-pushkar@users.noreply.github.com>
2021-08-19 15:22:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello From Amplify </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+  componentDidMount() {
+    const api = 'https://nzv8s9m7vk.execute-api.eu-west-1.amazonaws.com/staging';
+    const data = { "name" : "Mike" };
+    axios
+      .post(api, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  render() {
+    return (
+      <div>Medium Tutorial</div>
+    );
+  }
 }
-
 export default App;
